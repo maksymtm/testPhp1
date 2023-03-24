57aa82179299a3eb295992f78809b4e1dffd7ad5
maksym46113 <maksym46113@gmail.com>
2023-03-24 15:03:10 +0100

    Add Say HELLO FUNCTION

--- sklep/utils/CMS.php
+++ sklep/utils/CMS.php
@@ -1,6 +1,11 @@
 <?php
     require_once "Db.php";
     class CMS {
+
+        static function sayHello() {
+            echo "Hello World";
+        }
+
         static function getContent($id_key) {
             $language = isset($_COOKIE["language"]) ? $_COOKIE["language"] : "en";
             
